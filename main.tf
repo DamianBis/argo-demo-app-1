2604f0c10fa2681cf93c0f814b17fe573d58488c
DamianBis <daimob@gmail.com>
2023-05-24 21:37:09 +1000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,92 @@
+variable "clusterId" {}
+variable "vpcId" {}
+variable "environmentName" {}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "${var.app_name}-execution-task-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  tags = {
+    Name        = "${var.app_name}-iam-role"
+    Environment = var.environmentName
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "ghcr.io/damianbis/argo-demo-app-1:sha-b3b5f6a"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "service-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = var.vpcId
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "300"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/v1/status"
+    unhealthy_threshold = "2"
+  }
+
+  tags = {
+    Name        = "service-lb-tg"
+    Environment = var.environmentName
+  }
+}
+
+
+
+resource "aws_ecs_service" "myService" {
+  name            = "myService"
+  cluster         = var.clusterId
+  task_definition = aws_ecs_task_definition.service.arn
+  desired_count   = 1
+  iam_role        = aws_iam_role.ecsTaskExecutionRole.arn
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = "service"
+    container_port   = 80
+  }
+}
