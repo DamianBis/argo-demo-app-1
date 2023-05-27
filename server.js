49a88126a9ac939f2d4832993d0f6feb9b1e4f53
DamianBis <daimob@gmail.com>
2023-05-28 00:09:26 +1000

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
 app.get('/', (req, res) => {
-    res.status(200).send('hello world');
+    res.status(200).send('hello  world');
 });
 
 app.listen(config.port, config.host, (e)=> {
